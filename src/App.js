b16de2d933c04f83626f38dbbd252a92d64ce179
vedang999 <vedanglokhande2003@gmail.com>
2025-01-23 12:00:07 +0530

    resp commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ProgressBar from './ProgressBar';
 
 const App = () => {
   return (
-    <div className="App p-4">
+    <div className="App p-2">
       <div className="w-full lg:w-2/3">
         <ProgressBar />
       </div>
